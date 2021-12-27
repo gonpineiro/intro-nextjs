7193746ccd199257fe589eca09ae35f12d17ee8f
Gonzalo <gonzalo@estadodigital.com>
2021-12-27 16:06:57 -0300

    Armando API con un solo elemento, y preparando el debug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar/Navbar'
 
 const HomePage = () => {
+  const [productList, setProductList] = useState<TProduct[]>([])
+
+  useEffect(() => {
+    fetch('/api/avo')
+      .then((response) => response.json())
+      .then(({ data, lenght }) => {
+        setProductList(data)
+      })
+  }, [])
+
   return (
     <div>
       <Navbar />
       <div>Next.js!</div>
+      {productList.map((product) => (
+        <div>{product.name}</div>
+      ))}
     </div>
   )
 }
